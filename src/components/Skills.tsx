1a5e1477e2ba930ea60732ba31d1bf00829b7f26
adipurnamk <muhammad.adipurna.k@gmail.com>
2025-01-10 16:41:59 +0700

    Refactor Skills component to remove placeholder comments and enhance icon styling
    - Removed comments indicating the use of DiGit as placeholders for GitLab, Bitbucket, ArgoCD, and FluxCD.
    - Updated icon styling for better visual consistency in the Skills component.

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,11 +5,10 @@ import { FaGithub } from "react-icons/fa";
 
 const Skills = () => {
   const skills = [
-    // Grouping impactful and similar skills together
     { name: 'Git', icon: <DiGit /> },
     { name: 'GitHub', icon: <FaGithub /> },
-    { name: 'GitLab', icon: <DiGit /> }, // Using DiGit as a placeholder for GitLab
-    { name: 'Bitbucket', icon: <DiGit /> }, // Using DiGit as a placeholder for Bitbucket
+    { name: 'GitLab', icon: <DiGit /> },
+    { name: 'Bitbucket', icon: <DiGit /> },
     { name: 'Linux', icon: <DiLinux /> },
     { name: 'Docker', icon: <DiDocker /> },
     { name: 'Kubernetes', icon: <SiKubernetes /> },
@@ -17,8 +16,8 @@ const Skills = () => {
     { name: 'GCP', icon: <DiGoogleCloudPlatform /> },
     { name: 'Azure', icon: <VscAzure /> },
     { name: 'Terraform', icon: <SiTerraform /> },
-    { name: 'ArgoCD', icon: <DiGit /> }, // Using DiGit as a placeholder for ArgoCD
-    { name: 'FluxCD', icon: <DiGit /> }, // Using DiGit as a placeholder for FluxCD
+    { name: 'ArgoCD', icon: <DiGit /> },
+    { name: 'FluxCD', icon: <DiGit /> },
     { name: 'Jenkins', icon: <DiJenkins /> },
     { name: 'Python', icon: <DiPython /> },
     { name: 'Bash', icon: <SiGnubash /> },
@@ -36,7 +35,7 @@ const Skills = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-3xl mx-auto">
           {skills.map((skill, index) => (
             <div key={index} className="flex items-center space-x-2 p-4 border rounded-lg shadow-md">
-              <span className="text-2xl">{skill.icon}</span>
+              <span className="text-2xl text-gray-700 dark:text-gray-300">{skill.icon}</span>
               <span className="text-gray-700 dark:text-gray-300">{skill.name}</span>
             </div>
           ))}
